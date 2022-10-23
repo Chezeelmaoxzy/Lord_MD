03e2d72b34bc20a8c69dd0ad4709dea6e25d7908
Chezeelmaoxzy <114744308+Chezeelmaoxzy@users.noreply.github.com>
2022-10-23 09:35:16 +0800

    config.js

--- config.js
+++ config.js
@@ -44,37 +44,37 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/lord_ammar_quoteser'
-global.sgh = 'https://github.com/AmmarrBN'
-global.sgc = 'https://chat.whatsapp.com/BYcW7rue6fA8gNdyLPedDe'
+global.sig = 'https://instagram.com/Azmihaqqan67'
+global.sgh = 'https://github.com/Chezeelmaoxzy'
+global.sgc = '-'
 global.sdc = '-'
-global.snh = 'https://youtube.com/channel/UCFeZ5BGt8lbOZwIj2MNOlIQ'
+global.snh = 'https://youtube.com/channel/UCcd8Mmh9mQzQhfZJPwnOWwQ'
 
 /*============== PAYMENT ==============*/
-global.pdana = '085712420674'
-global.povo = '085712420674'
-global.pgopay = '085712420674'
-global.ppulsa = '087708773367'
-global.ppulsa2 = '088229683561'
-global.psaweria = 'https://saweria.com/kannabotz'
+global.pdana = '081241956878'
+global.povo = '-'
+global.pgopay = '081241956878'
+global.ppulsa = '081241956878'
+global.ppulsa2 = '082195750041'
+global.psaweria = '-'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6287708773367'
-global.nomorown = '6288229683561'
-global.namebot = 'Lord-Botz'
-global.nameown = 'Ammar-Executed'
+global.nomorbot = '6282195750041'
+global.nomorown = '6281241956878'
+global.namebot = 'wangsaff bot'
+global.nameown = 'azmy'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6283143393763'],
-  ['6285712420674'],
-  ['6287708773367'],
-  ['6288229683561', '⌬ Ammar Ganz', true]
+  ['6281241956878'],
+  ['6281241956878'],
+  ['6281241956878'],
+  ['6281241956878', '⌬ azmy', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['6288229683561'] // Want some help?
-global.prems = ['6288229683561','6287708773367'] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
+global.mods = ['6281241956878'] // Want some help?
+global.prems = ['6281241956878','6281241956878'] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
 
 
 /*============== API ==============*/
@@ -116,13 +116,13 @@ global.APIKeys = { // APIKey Here
 
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 Lord Botz ⁩㊣ 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Lord - Khun˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ Lord 𝗕𝗢𝗧'
+global.wm = '                「 Wangsaff bot ⁩㊣ 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘ created by azmy  ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ Wangsaff bot'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
 global.titlebot = `⫹⫺ RPG BOT Whatsapp | By ${global.nameown}`
-global.author = '                「 Lord BOT あ⁩ 」'
+global.author = '                「 wangsaff bot あ⁩ 」'
 
 
 /*============== LOGO ==============*/
@@ -189,8 +189,8 @@ global.lolm = 'Ⓛ'
 global.htjava = '⫹⫺' //hiasan Doang :v
 
 /*============== STICKER WM ==============*/
-global.stickpack = '.'
-global.stickauth = `☂︎ Lord Botz\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
+global.stickpack = 'wangsaffbot'
+global.stickauth = `MD`
 
 global.multiplier = 38 // The higher, The harder levelup
 
